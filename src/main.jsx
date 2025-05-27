bb0ff1672e8a2948702d5fe04934ddbbcdb5a73c
hitman2303 <lautarovallejos14@gmail.com>
2025-05-27 17:53:35 -0300

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router-dom';  // 👈 CAMBIA ESTO
 import { CartProvider } from './context/CartContext.jsx';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>  {/* 👈 CAMBIE ESTO A VER SI LO AGARRA GITHUB */}
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )

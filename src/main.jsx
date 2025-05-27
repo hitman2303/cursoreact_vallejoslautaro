92b2fe18561522aaed00136c281b72ffc49741a8
hitman2303 <lautarovallejos14@gmail.com>
2025-05-26 21:29:33 -0300

    cambio

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router';
-import { CartProvider } from './context/Cartcontext.jsx';
+import { CartProvider } from './context/CartContext.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>

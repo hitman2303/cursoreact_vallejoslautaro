523bc34527cc03d07806a6a8eb22404b4a841ca4
hitman2303 <lautarovallejos14@gmail.com>
2025-07-15 10:26:51 -0300

    Set base './' for Vite in production

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
-  base: './', // ← ¡ESTO ES CLAVE!
+  base: './',  // <- clave para que las rutas JS comiencen con "./assets/..."
 });

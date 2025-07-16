99f0b5209d729afd0a62d0b36a6792649cd166b0
hitman2303 <lautarovallejos14@gmail.com>
2025-07-16 11:02:41 -0300

    Versión final funcionando para Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { CartProvider } from "./context/CartContext";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider> {/* 👈 envolver toda la app */}
+    <CartProvider>
       <HashRouter>
         <App />
       </HashRouter>
